2ad03a3e2c5e6c653fbc6ef49de7a8858135c199
satex <satexpconline@gmail.com>
2025-12-11 21:29:38 +0300

    ilk yükleme

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,6 +1,8 @@
 
 import express from "express";
 
+
+
 const router = express.Router();
     router.get("/send", (req, res) => {
     res.send("Message endpoind");
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 
-
+import connectDB from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import connectDB from "./lib/db.js";
+
 
 dotenv.config();
 const app = express();
